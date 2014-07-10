bf3348d46264cc06a72db42ddc97cb9ba5fc347d
Rafael Tavares Andrade <rafaeltavaresandrade@gmail.com>
2014-07-10 15:42:52 -0300

    Adicionando projeto
    Xamarin

---
+++ PrimeiroAppXam/AndroidApp/Resources/classes/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SharedApp;
+
+namespace AndroidApp.classes
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    { 
+       
+        SharedMainActivity AtivShared = new SharedMainActivity();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            
+            base.OnCreate(bundle);
+            // seta o layout que será usado
+            SetContentView(Resource.Layout.Main);
+            // pega o botão do layout
+            Button buttonContador = FindViewById<Button>(Resource.Id.MyButton);
+            buttonContador.Click += delegate { buttonContador.Text = string.Format("{0} clicks!", AtivShared.AumentarValor()); };
+
+            Button buttonLink = FindViewById<Button>(Resource.Id.Link);
+
+            buttonLink.Click += delegate { };
+        }
+    }
+}
+
---
+++ PrimeiroAppXam/SharedApp/SharedMainActivity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedApp
+{
+    public class SharedMainActivity
+    {
+        #region Variáveis
+        public int valor { get; set; }
+        #endregion
+
+        #region Aumentar Valor do Click
+        public int AumentarValor()
+        {
+            return valor = valor + 1;
+        }
+        #endregion
+    }
+
+}
